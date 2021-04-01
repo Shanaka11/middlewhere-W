b043892548247a7f205f0c785533fa09e1a4eaeb
Shanaka Abeysinghe <shanaka@localhost.localdomain>
2021-04-01 14:50:40 +0530

    Basic Functionality Implemented

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const axios = require("axios");
 const { create, convert } = require("xmlbuilder2");
+const fetch = require("node-fetch");
+const https = require("https");
 
 const app = express();
 app.use(express.json());
 
-app.get("/test/:url", (request, response) => {
-  const url = request.params.url;
+app.get("/test/", async (request, response) => {
+  const instance = axios.create({
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false,
+    }),
+  });
 
+  // const baseUrl = request.params.url;
+  const baseUrl = request.query.url;
+  const url = baseUrl.substring(1, baseUrl.length - 1);
+
+  const server = "https://20.195.101.222/api/";
+  console.log(url);
   //   const headers = {
   //     "Content-Type": "application/json",
   //     "X-API-Key": process.env.ARCEO_SCAN_API_KEY
@@ -17,30 +29,58 @@ app.get("/test/:url", (request, response) => {
   //     { headers }
   //   );
 
-  const data = {
-    PFS_Quote_Import: {
-      "@xmlns": "https://integration.ipfs.com/IntegrationQuoting",
-      Header: {
-        System: "MCGCYB",
+  try {
+    const userData = {
+      User: {
+        login_id: "cenmetrixa",
+        password: "cenmtx@1",
       },
-      Security: {
-        Login: "mcgowancyber",
-        AccessCode: "69C6Z3C",
-      },
-    },
-  };
+    };
+    // const loginHeader = {
+    //   accept: "application/json",
+    //   "Content-Type": "application/json",
+    // };
 
-  const doc = create(data);
-  const finalXml = doc.end();
+    const { headers: resHeaders } = await instance.post(
+      `${server}login`,
+      userData
+      //   loginHeader
+    );
 
-  response.set("Content-Type", "text/xml");
-  response.send(finalXml);
+    const reqHeaders = {
+      "bs-session-id": resHeaders["bs-session-id"],
+    };
+    // console.log(reqHeaders);
+    const { data } = await instance.get(`${server}${url}`, {
+      headers: reqHeaders,
+    });
+
+    const doc = create({ responseXml: data });
+    const finalXml = doc.end();
+
+    response.set("Content-Type", "text/xml");
+    response.send(finalXml);
+    // console.log(data);
+    return;
+  } catch (error) {
+    // console.log(error.request);
+    if (error.response.data) {
+      // console.log(error.response.data);
+      response.send(error.response.data);
+    } else {
+      // console.log(error);
+      response.send(error);
+    }
+    return;
+  }
 });
 
 // listen for requests :)
 // const listener = app.listen(process.env.PORT, () => {
 //   console.log("Your app is listening on port " + listener.address().port);
 // });
+// Request Template
+// http://localhost:8081/test/?url=%22api/device_groups%22
 const listener = app.listen(8081, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
